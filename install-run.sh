f2e4b3ec0558cccfe9eba6c76beb1accb701598a
Vishawanath kokare <vishkokre@gmail.com>
2020-05-22 11:50:37 +0530

    Create install-run.sh

---
+++ install-run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+clear
+
+# that my printng logo on terminal
+
+echo "#     # # ######## #      # ######## ####### ########"
+echo "#     # # ######## #      # #      # #              #"
+echo "#     # # #        #      # #      # #              #"
+echo "#     # # #        #      # #      # #              #"
+echo "#     # # ######## ######## ######## ####### ########"
+echo "#     # # ######## #      #        #       #        #"
+echo "#    #  #       ## #      #        #       #        #"
+echo " #  #   # ######## #      #        #       #        #"
+echo "  ##    # ######## #      # ######## ####### ########"
+
+sleep 5
+
+echo "$############THIS WAS MADE BY VISHAWANATH###############"
+echo "$########https://www.github.com/VISHAWANATH953##########"
+
+# all things for install docker and add docker in boot also start docker service and finally install this termux docker image
+
+apk add docker && clear && rc-update add docker boot && clear && service docker start && clear && docker run -it xeffyr/termux:latest
